73949c6e7410b4ff540df6d65e8c65434e9f4c83
ZeekoZhu <vaezt@outlook.com>
2017-12-04 11:03:47 +0800

    减少内存使用

--- Program.fs
+++ Program.fs
@@ -13,10 +13,12 @@ open System.Text
 type CommentWriter() = 
     
     let agent = 
-        MailboxProcessor<Comment seq>.Start(fun inbox -> 
+        MailboxProcessor<Comment list>.Start(fun inbox -> 
             let rec messageLoop() = 
                 async { 
                     let! comments = inbox.Receive()
+                    if comments.Length > 1000 then 
+                        do printfn "%d has %d comments" comments.Head.ArticleId comments.Length
                     let sb = StringBuilder()
                     let aggregate comment = 
                         sb.AppendLine(JsonConvert.SerializeObject(comment, Formatting.None) + ",") |> ignore
@@ -38,16 +40,23 @@ let main _ =
         async { 
             try 
                 let! comments = GetCommentsOf id |> AsyncSeq.toListAsync
-                printfn "%d" comments.Length
                 writer.Save comments
             with Failure e -> failwith e
         }
     File.WriteAllText("./data.json", "[")
-    [ articleId..(articleId + 1) ]
-    |> List.map makeTask
-    |> Async.Parallel
-    |> Async.RunSynchronously
-    |> ignore
+    let rec batchDownload (range : int list) = 
+        if range.Length > 0 then 
+            range
+            |> List.take 200
+            |> List.map makeTask
+            |> Async.Parallel
+            |> Async.RunSynchronously
+            |> ignore
+            if range.Length <= 200 then range
+            else range |> List.skip 200
+            |> batchDownload
+        else ()
+    [ articleId..(articleId + 1000) ] |> batchDownload
     File.AppendAllText("./data.json", "]")
     printfn "%s" "complete!"
     0
