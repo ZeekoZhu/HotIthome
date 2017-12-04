90df2ab39f83a716d05204f8721fab976cfc0c22
ZeekoZhu <vaezt@outlook.com>
2017-12-04 10:35:11 +0800

    改进并发性能

--- Program.fs
+++ Program.fs
@@ -8,20 +8,46 @@ open IthomeSpider
 open Newtonsoft.Json
 open System
 open System.IO
-open System.Text.RegularExpressions
+open System.Text
+
+type CommentWriter() = 
+    
+    let agent = 
+        MailboxProcessor<Comment seq>.Start(fun inbox -> 
+            let rec messageLoop() = 
+                async { 
+                    let! comments = inbox.Receive()
+                    let sb = StringBuilder()
+                    let aggregate comment = 
+                        sb.AppendLine(JsonConvert.SerializeObject(comment, Formatting.None) + ",") |> ignore
+                    comments |> Seq.iter aggregate
+                    do! File.AppendAllTextAsync("./data.json", sb.ToString()) |> Async.AwaitTask
+                    return! messageLoop()
+                }
+            messageLoop())
+    
+    member this.Save comments = agent.Post comments
 
 [<EntryPoint>]
 let main _ = 
     printfn "Hello World from F#!"
     let articleId = 326995
-    async { 
-        for id in [ articleId..(articleId + 10000) ] do
+    let writer = CommentWriter()
+    
+    let makeTask id = 
+        async { 
             try 
                 let! comments = GetCommentsOf id |> AsyncSeq.toListAsync
-                for c in comments do
-                    File.AppendAllText("./data.json", JsonConvert.SerializeObject(c, Formatting.None) + "\n")
-            with _ -> ()
-    }
+                printfn "%d" comments.Length
+                writer.Save comments
+            with Failure e -> failwith e
+        }
+    File.WriteAllText("./data.json", "[")
+    [ articleId..(articleId + 1) ]
+    |> List.map makeTask
+    |> Async.Parallel
     |> Async.RunSynchronously
+    |> ignore
+    File.AppendAllText("./data.json", "]")
     printfn "%s" "complete!"
     0
